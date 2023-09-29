1a0c36fee95ba74f5a47bb5b9f1b23c45fae17df
Aryo Aditya <aryoaditya2000@gmail.com>
2023-09-29 16:32:58 +0700

    repositories

---
+++ API/Controllers/UniversityController.cs
@@ -0,0 +1,83 @@
+﻿using API.Contracts;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UniversityController : ControllerBase
+    {
+        private readonly IUniversityRepository _universityRepository;
+
+        public UniversityController(IUniversityRepository universityRepository)
+        {
+            _universityRepository = universityRepository;
+        }
+
+        // HTTP GET untuk mengambil semua data Universitas
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _universityRepository.GetAll();
+            if (!result.Any())
+            {
+                return NotFound("Data Not Found"); // Mengembalikan pesan jika tidak ada data yang ditemukan
+            }
+
+            return Ok(result);  // Mengembalikan data Universitas jika ada
+        }
+
+        // HTTP GET untuk mengambil data Universitas berdasarkan GUID
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var result = _universityRepository.GetByGuid(guid);
+            if (result is null)
+            {
+                return NotFound("Id Not Found"); // Mengembalikan pesan jika ID tidak ditemukan
+            }
+            return Ok(result);  // Mengembalikan data Universitas jika ditemukan
+        }
+
+        // HTTP POST untuk membuat data Universitas baru
+        [HttpPost]
+        public IActionResult Create(University university)
+        {
+            var result = _universityRepository.Create(university);
+            if (result is null)
+            {
+                return BadRequest("Failed to create data"); // Mengembalikan pesan jika gagal membuat data
+            }
+
+            return Ok(result); // Mengembalikan data Universitas yang baru saja dibuat
+        }
+
+        // HTTP PUT untuk memperbarui data Universitas berdasarkan GUID
+        [HttpPut("{guid}")]
+        public IActionResult Update(University university)
+        {
+            var result = _universityRepository.Update(university);
+            if (!result)
+            {
+                return BadRequest("Failed to update data");  // Mengembalikan pesan jika gagal memperbarui data
+            }
+
+            return Ok("Data updated successfully"); // Mengembalikan pesan sukses jika pembaruan berhasil
+        }
+
+        // HTTP DELETE untuk menghapus data Universitas berdasarkan GUID
+        [HttpDelete("{guid}")]
+        public IActionResult Delete(University university)
+        {
+            var result = _universityRepository.Delete(university);
+            if (!result)
+            {
+                return BadRequest("Failed to delete data"); // Mengembalikan pesan jika gagal menghapus data
+            }
+
+            return Ok("Data deleted successfully"); // Mengembalikan pesan sukses jika penghapusan berhasil
+        }
+
+    }
+}
---
+++ API/Repositories/UniversityRepository.cs
@@ -0,0 +1,74 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories
+{
+    public class UniversityRepository : IUniversityRepository
+    {
+        private readonly BookingManagementDbContext _context;
+
+        // Constructor
+        public UniversityRepository(BookingManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        // Membuat entitas Universitas baru dalam database
+        public University? Create(University university)
+        {
+            try
+            {
+                _context.Set<University>().Add(university);
+                _context.SaveChanges();
+                return university; // Mengembalikan universitas yang baru saja dibuat
+            }
+            catch
+            {
+                return null; // Mengembalikan null jika terjadi kesalahan
+            }
+        }
+
+        // Menghapus entitas Universitas dari database
+        public bool Delete(University university)
+        {
+            try
+            {
+                _context.Set<University>().Remove(university);
+                _context.SaveChanges();
+                return true; // Mengembalikan true jika penghapusan berhasil
+            }
+            catch
+            {
+                return false; // Mengembalikan false jika terjadi kesalahan
+            }
+        }
+
+        // Mendapatkan semua entitas Universitas dalam database
+        public IEnumerable<University> GetAll()
+        {
+            return _context.Set<University>().ToList(); // Mengembalikan daftar universitas dalam bentuk List
+        }
+
+        // Mendapatkan entitas Universitas berdasarkan GUID
+        public University GetByGuid(Guid guid)
+        {
+            return _context.Set<University>().Find(guid);
+        }
+
+        // Memperbarui entitas Universitas dalam database
+        public bool Update(University university)
+        {
+            try
+            {
+                _context.Set<University>().Update(university);
+                _context.SaveChanges();
+                return true; // Mengembalikan true jika pembaruan berhasil
+            }
+            catch
+            {
+                return false; // Mengembalikan false jika terjadi kesalahan
+            }
+        }
+    }
+}
